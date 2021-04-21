93678a000790fb424e76e568cc9f8438dced264d
Haseeb-21 <71402178+Haseeb-21@users.noreply.github.com>
2021-04-21 15:10:33 -0700

    Updated hard coded database values

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 let Database = {
     cindy: {
-        reminders: [{id: 1, title: "abc", description: "abcabc", subtask: "Eat food", date: "April 4, 2021", completed: false}]
+        reminders: [{id: 1, title: "abc", description: "abcabc", subtask: "Eat food", date: "April 4, 2021", tag: "school", completed: false}]
     },
     alex: {
         reminders: []
